86dd21ba7a762d8501d9573b8134ccc7a2d96c27
rammelion <61567322+rammelion@users.noreply.github.com>
2025-03-07 08:01:51 +0100

    removed the game

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Home from './components/pages/Home';
-import MemoryGames from './components/pages/MemoryGames';
 import './App.css';
 
 function App() {
   return (
     <>
-        <nav>
-            <Link to='/' className='nav-item'>Home</Link>
-            <Link to='/memorygame' className='nav-item'>Memory Games</Link>
-        </nav>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/memorygame' element={<MemoryGames />} />
-        </Routes>
+        <Home />
     </>
   );
 }
